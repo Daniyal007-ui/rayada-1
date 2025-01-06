b64516dd23b531e2d43a8b39b2ed9dc0954ec2e4
daniyal007-ui <daniyalkumroh007@gmail.com>
2025-01-06 14:55:35 +0500

    first commit

--- backened/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();  // Load environment variables
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
